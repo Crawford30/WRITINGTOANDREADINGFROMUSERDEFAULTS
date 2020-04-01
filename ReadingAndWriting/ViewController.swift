e9ccd84ee87f0186a046ee8b06466546a996cc24
joel <joelcrawford30@gmail.com>
2020-04-01 19:53:34 +0300

    Initial Commit

---
+++ ReadingAndWriting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReadingAndWriting
+//
+//  Created by JOEL CRAWFORD on 4/1/20.
+//  Copyright © 2020 JOEL CRAWFORD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
